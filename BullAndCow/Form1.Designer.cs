01d6f9a98c9c231dd4afda8da523ba9405f699ae
Boyan Mihailov <mihailovy@gmail.com>
2025-02-10 13:45:53 +0200

    Initial commit of code

---
+++ BullAndCow/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace BullAndCow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Вашето число:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(25, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(244, 224);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 138);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Играй";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 313);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Бикове и крави";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BullAndCow/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BullAndCow
+{
+    public partial class Form1 : Form
+    {
+        private int[] number = new int[4];
+        private int[] numberEntered = new int[4];
+
+        public int[] GenerateNumber()
+        {
+            var rand = new Random();
+            
+            number[0] = rand.Next(1, 9);
+            while (number[1] == number[0])
+            {
+                number[1] = rand.Next(1, 9);
+            }
+            while (number[2] == number[1] || number[2] == number[0])
+            {
+                number[2] = rand.Next(1, 9);
+            }
+            while (number[3] == number[2] || number[3] == number[1] || number[3] == number[0])
+            {
+                number[3] = rand.Next(1, 9);
+            }
+            textBox1.Text = "" + (1000 * number[0] + 100 * number[1] + 10 * number[2] + number[3]);
+            return number;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int remainder; 
+            int n = int.Parse(textBox1.Text);
+            numberEntered[0] = n / 1000;
+            remainder = n % 1000;
+            numberEntered[1] = remainder / 100;
+            remainder = remainder % 100;
+            numberEntered[2] = remainder / 10;
+            remainder = remainder % 10;
+            numberEntered[3] = remainder;
+        }
+    }
+}
